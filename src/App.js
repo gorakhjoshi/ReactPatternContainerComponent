852c1296065d76f47ae14f7a0dac4244961053e7
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-12-08 10:35:39 +0545

    UserLoader component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { CurrentUserLoader } from './CurrentUserLoader';
+import { UserLoader } from './UserLoader';
 import { UserInfo } from './UserInfo';
 
 function App() {
   return (
-    <CurrentUserLoader>
-      <UserInfo />
-    </CurrentUserLoader>
+    <>
+      <UserLoader userId='123'>
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId='234'>
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId='345'>
+        <UserInfo />
+      </UserLoader>
+    </>
   );
 }
 
